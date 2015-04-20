984eaf7541286143104d402e6a8082565a7ff0eb
Patrick Beukema <beuk.pat@gmail.com>
2015-04-20 00:12:25 -0400

    Changed incorrect signal back to circle.

--- r2d4_practice.py
+++ r2d4_practice.py
@@ -96,12 +96,12 @@ practiceFeedback = visual.TextStim(win=win, ori=0, name='text_4',
 #     depth=-6.0)
 
 
-#Wrong_1 = visual.Circle(win=win, units = 'pix', radius = 100,lineColor='red', fillColor = 'red')
-Wrong_1 = visual.TextStim(win=win, ori=0, name='Right_1',
-    text='X',    font='Arial',
-    pos=[0, 0], height=.5, wrapWidth=None,
-    color='red', colorSpace='rgb', opacity=1,
-    depth=-6.0)
+Wrong_1 = visual.Circle(win=win, units = 'pix', radius = 100,lineColor='red', fillColor = 'red')
+# Wrong_1 = visual.TextStim(win=win, ori=0, name='Right_1',
+#     text='X',    font='Arial',
+#     pos=[0, 0], height=.5, wrapWidth=None,
+#     color='red', colorSpace='rgb', opacity=1,
+#     depth=-6.0)
 
 
 # Initialize components for Routine "Begin_Blocks"
@@ -138,6 +138,65 @@ text = visual.TextStim(win=win, ori=0, name='text',
     pos=[0, 0], height=0.1, wrapWidth=None,
     color=u'white', colorSpace='rgb', opacity=1,
     depth=0.0)
+
+
+##Generate practice stimuli:
+block_loops = pd.DataFrame()
+block_loops['Block_id'] = ['block_1_img','block_2_img' ]
+block_loops['Block_ans'] = ['block_1_ans','block_2_ans' ]
+block_loops.to_csv('practice_blocks.csv', index= False)
+
+img_dict = {2: 'image_folder/stim_2.png', 3: 'image_folder/stim_3.png', 4: 'image_folder/stim_4.png', 5: 'image_folder/stim_5.png'}
+key_dict = {2:'2', 3:'3', 4:'4', 5:'5'}#key mapping
+length = 256 # number of trials within a  block should be 256
+dfStims = pd.DataFrame()
+block_ids = [1, 1] #1 is random #2 is sequence
+
+#Generate Pseudo-Random Stimuli Ordering
+def genRandom(length):
+    random_stims = []
+    random_img_ids = []
+    random_ans = []
+    for x in range(0,length):
+        if len(random_stims) == 0:
+            random_stims.append(randint(2,5))
+        elif len(random_stims) > 0:
+            val = randint(2,5)
+            while val == random_stims[x-1]:
+                val = randint(2,5)
+            random_stims.append(val)
+        random_img_ids.append(img_dict[random_stims[x]])
+        random_ans.append(key_dict[random_stims[x]])
+    random_img_ids = np.asarray(random_img_ids)
+    random_ans= np.asarray(random_ans)
+    return (random_img_ids, random_ans)
+
+#Generate Sequence Ordering --.
+def genSequence(length):
+    #Generate Sequence Stimuli Ordering.
+    sequence_stims = [4,5,3,4,2,5,3,2,4,5,4,5,2,4,5,3,2,3,5,3,4,2,3,2,3,5,4,2,4,2,3,5]
+    rota_ind = randint(1,31)
+    sequence_stims = sequence_stims[rota_ind:]  + sequence_stims[:rota_ind]
+    sequence_img_ids = []
+    sequence_ans = []
+    sequence_stims= np.tile(sequence_stims,8)
+    for x in range(0,length):
+        sequence_img_ids.append(img_dict[sequence_stims[x]])
+        sequence_ans.append(key_dict[sequence_stims[x]])
+    return (sequence_img_ids, sequence_ans)
+
+for type in range(0,len(block_ids)):
+    if block_ids[type] == 1:
+        img_ids, ran_ans = genRandom(length)
+        dfStims['block_'+str(type+1)+'_img'] = img_ids
+        dfStims['block_'+str(type+1)+'_ans'] = ran_ans
+    elif block_ids[type] == 2:
+        img_ids, seq_ans = genSequence(length)
+        dfStims['block_'+str(type+1)+'_img'] = img_ids
+        dfStims['block_'+str(type+1)+'_ans'] = seq_ans
+
+dfStims.to_csv('practice_stims.csv', index= False)
+
 
 # Create some handy timers
 globalClock = core.Clock()  # to track the time since experiment started
@@ -408,6 +467,8 @@ Block_Loop = data.TrialHandler(nReps=1, method='sequential',
     extraInfo=expInfo, originPath=None,
     trialList=data.importConditions(u'practice_blocks.csv'),
     seed=None, name='Block_Loop')
+
+print Block_Loop.trialList
 thisExp.addLoop(Block_Loop)  # add the loop to the experiment
 thisBlock_Loop = Block_Loop.trialList[0]  # so we can initialise stimuli with some values
 # abbreviate parameter names if possible (e.g. rgb=thisBlock_Loop.rgb)
@@ -431,7 +492,7 @@ for thisBlock_Loop in Block_Loop:
     # set up handler to look after randomisation of conditions etc
     trials = data.TrialHandler(nReps=1, method='sequential',
         extraInfo=expInfo, originPath=None,
-        trialList=data.importConditions(u'practice_stim.csv'),
+        trialList=data.importConditions(u'practice_stims.csv'),
         seed=None, name='trials')
     thisExp.addLoop(trials)  # add the loop to the experiment
     thisTrial = trials.trialList[0]  # so we can initialise stimuli with some values
