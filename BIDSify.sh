d62b3ebc2233266f93ccb9016ec1363fe81998cf
Patrick Beukema <beuk.pat@gmail.com>
2018-01-25 11:21:05 -0500

    Update BIDSify.sh

--- BIDSify.sh
+++ BIDSify.sh
@@ -1,6 +1,6 @@
-#! /bin/bash
+#!/usr/bin/env bash
 
-# We are aiming for this file structure (see the docs for details) http://bids.neuroimaging.io/bids_spec1.0.2.pdf
+# We are aiming for the following file structure (see the docs for details) http://bids.neuroimaging.io/bids_spec1.0.2.pdf
 #  /source_data
 #   README
 #   participants.tsv
@@ -34,15 +34,14 @@
 # This entire process takes about 5 minutes per subject - bottle neck is defacing followed by gzipping. 
 # Final database size is ~30 GB. 
 
-fmap_name=fieldmap_96_AP_2mmIsoVoxel_192FOV_66Slices
-t1_name=t1_mpr_sag_iso_2pat_9degflip_TI900
-t2_name=t2_spc_sag_p2_iso
+fmap_name='fieldmap_96_AP_2mmIsoVoxel_192FOV_66Slices'
+t1_name='t1_mpr_sag_iso_2pat_9degflip_TI900'
+t2_name='t2_spc_sag_p2_iso'
 id_count=1
 n_subs=18 # change to your specific n_subs
 nruns=6 # how many independent runs per scanning session 
-for subject in $(seq 1 $n_subs)
-
-do
+((i = 0; i < n; i++))
+for ((subject = 0; subject < n_subs; subject++)); do
   printf -v bids_id "%02d" ${id_count} #left padding with zeros for BIDS compliancy
   # Change this dir structure to your preference e.g. bids_${study_name} o/w you want the exact same naming conventions
   mkdir /data/bids_r2d4/sub-${bids_id}
@@ -66,8 +65,7 @@ do
       ;;
     esac
 
-    for run in $(seq 1 $nruns)
-    do
+    for ((run = 0; run < n_runs; run++)); do
       cp /data/r2d4/subjects/${subject}_${count}/RER_Run${run}_01/RER_Run${run}_01.nii \
       /data/bids_r2d4/sub-${bids_id}/ses-${session}/func/sub-${bids_id}_ses-${session}_task-cuedSFM_run-0${run}_bold.nii
       gzip /data/bids_r2d4/sub-${bids_id}/ses-${session}/func/sub-${bids_id}_ses-${session}_task-cuedSFM_run-0${run}_bold.nii
@@ -106,10 +104,9 @@ do
   pydeface /data/bids_r2d4/sub-${bids_id}/ses-pre/anat/sub-${bids_id}_ses-pre_T2w.nii \
   --outfile /data/bids_r2d4/sub-${bids_id}/ses-pre/anat/sub-${bids_id}_ses-pre_T2w.nii
 
-
   # BIDS prefers compression again
   gzip /data/bids_r2d4/sub-${bids_id}/ses-pre/anat/sub-${bids_id}_ses-pre_T1w.nii
   gzip /data/bids_r2d4/sub-${bids_id}/ses-pre/anat/sub-${bids_id}_ses-pre_T2w.nii
 
-  let id_count=id_count+1 #takes about 5 minutes per subject to do all of this on my machine.  
+ ((id_count++)) #takes about 5 minutes per subject to do all of this on my machine.  
 done
